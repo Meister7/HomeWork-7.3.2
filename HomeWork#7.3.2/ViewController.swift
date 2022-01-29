54ce35fb0e9c61b51ab1cf9a681b248299845b35
Meister7 <emirkarmyshev7@gmail.com>
2022-01-29 13:20:28 +0600

    Initial Commit

---
+++ HomeWork#7.3.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork#7.3.2
+//
+//  Created by Эмир Кармышев on 29/1/22.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-HomeWork#7.3.2-HomeWork#7.3.2UITests/Pods-HomeWork#7.3.2-HomeWork#7.3.2UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HomeWork_7_3_2_HomeWork_7_3_2UITests : NSObject
+@end
+@implementation PodsDummy_Pods_HomeWork_7_3_2_HomeWork_7_3_2UITests
+@end
